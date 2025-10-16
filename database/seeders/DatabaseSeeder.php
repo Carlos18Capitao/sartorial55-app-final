a2cebc1a3f06f8c6f6e42fc16e330f851e24d952
Carlos Capitão <carlos18mauro@gmail.com>
2025-10-16 10:35:20 +0100

    chamada dos models.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Cliente;
+use App\Models\Encomenda;
+use App\Models\Item;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -21,16 +24,16 @@ class DatabaseSeeder extends Seeder
         ]);
 
         // Criar 10 clientes com encomendas
-        \App\Models\Cliente::factory(10)->create()->each(function ($cliente) {
+        Cliente::factory(10)->create()->each(function ($cliente) {
             // Cada cliente tem pelo menos 1 encomenda
             $numEncomendas = rand(1, 3);
             $cliente->encomendas()->saveMany(
-                \App\Models\Encomenda::factory($numEncomendas)->make()
+                Encomenda::factory($numEncomendas)->make()
             )->each(function ($encomenda) {
                 // Cada encomenda tem pelo menos 1 item
                 $numItens = rand(1, 5);
                 $encomenda->itens()->saveMany(
-                    \App\Models\Item::factory($numItens)->make()
+                    Item::factory($numItens)->make()
                 );
             });
         });
