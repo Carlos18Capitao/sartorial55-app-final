b90fdf7aaad3c28bb70950bb1246b2ce44d05535
Carlos Capitão <carlos18mauro@gmail.com>
2026-03-19 12:27:06 +0100

    remoção do cabeçalho

--- resources/js/Pages/Dashboard-app.vue
+++ resources/js/Pages/Dashboard-app.vue
@@ -655,17 +655,6 @@ import { Link } from '@inertiajs/vue3';
                     <!-- start page title -->
                     <div class="row">
                         <div class="col-12">
-                            <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                                <h4 class="mb-sm-0">Starter</h4>
-
-                                <div class="page-title-right">
-                                    <ol class="breadcrumb m-0">
-                                        <li class="breadcrumb-item"><a href="javascript: void(0);">Pages</a></li>
-                                        <li class="breadcrumb-item active">Starter</li>
-                                    </ol>
-                                </div>
-
-                            </div>
                             <slot />
                         </div>
                     </div>
